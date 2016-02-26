93eabe363c68e6f4ba0a727faec8934fe6717bb0
honeydukes <9and3over4@gmail.com>
2016-02-26 10:36:29 +0800

    submit with documentation

--- ui.R
+++ ui.R
@@ -6,7 +6,12 @@
 shinyUI(pageWithSidebar(
   headerPanel("Temperature Conversion"),
   sidebarPanel( 
-    numericInput('temp', 'Temperature', value = 37, min = -150, max = 250, step = 0.5),
+    h4('Instruction'),
+    h5('Step 1: Slide to temperature'),
+    h5('Step 2: Choose conversion type'),
+    h5('Step 3: Click "Convert" button'),
+    h3('     '),
+    sliderInput('temp', 'Temperature', value = 37, min = -150, max = 250, step = 0.5),
     radioButtons("radio", "Choose one :",
                       list("from Fahrenheit to Celcius" = 1,
                         "from Celsius to Fahrenheit" = 2), selected = 1),
